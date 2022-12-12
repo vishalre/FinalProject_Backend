1e5dad84fba283a998b36d7a60f5fa27a0ff0398
Saiteja Kura <kurasaiteja@gmail.com>
2022-12-11 23:49:31 -0500

    removed unique constraint

--- Models/ProductsModel.js
+++ Models/ProductsModel.js
@@ -5,7 +5,7 @@ const schema = mongoose.Schema({
   imageUrl: { type: String, required: true },
   price: { type: String, required: true },
   manufacturer: { type: String },
-  asin: { type: String, unique: true },
+  asin: { type: String},
   country: { type: String },
   originalPrice: { type: Number, min: 0 },
   discount: { type: Number, min: 0 },
